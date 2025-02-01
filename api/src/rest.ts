5fc15d57aaf62b79fccc13638725f2c102f28650
Fábio Henrique <fabiohenriquesf8@gmail.com>
2025-01-31 23:48:57 -0300

    chore[be]: added cors

--- api/src/rest.ts
+++ api/src/rest.ts
@@ -1,5 +1,5 @@
 ﻿import Koa from "koa";
-import BodyParser from "koa-bodyparser";
+import { bodyParser } from "@koa/bodyparser";
 import { configRoutes } from "./rest/configs/config-routes";
 import { errorHandler } from "./rest/middlewares/errorHandler";
 
@@ -7,24 +7,15 @@ export function startServer() {
   const app = new Koa();
   app.use(errorHandler);
 
-  app.use(BodyParser());
+  app.use(bodyParser());
 
   configRoutes(app);
 
   app.use(async (ctx, next) => {
+    await next();
+
     ctx.response.status = 404;
     ctx.response.body = { message: "Recurso não encontrado" };
-
-    await next();
-  });
-
-  app.use(async (ctx, next) => {
-    await next();
-    ctx.response.headers = {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    };
   });
 
   app.listen(5001, () => {
--- api/src/rest/configs/config-routes.ts
+++ api/src/rest/configs/config-routes.ts
@@ -1,10 +1,19 @@
 ﻿import Router from "@koa/router";
 import Koa from "Koa";
+import cors from "@koa/cors";
 import allRoutes from "../routes";
 
 export function configRoutes(
   koaInstance: Koa
 ): Router<Koa.DefaultState, Koa.DefaultContext>[] {
+  koaInstance.use(
+    cors({
+      origin: "*",
+      allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      allowHeaders: ["Content-Type", "Authorization"],
+    })
+  );
+
   return allRoutes.map((route) => {
     koaInstance.use(route.routes());
     koaInstance.use(route.allowedMethods());
